fedc870029ad0481517eeaf00b7aec47a8f3376e
Michael Pande <michaelpande@gmail.com>
2017-02-08 15:55:30 +0100

    Cleanup etc.

--- main.js
+++ main.js
@@ -1,52 +1,73 @@
 "use strict"; 
 
+
 function getAppName(){
+  var nameElement = document.getElementsByClassName('apphub_AppName');
+  
+  if(nameElement.length === 0) {
+    return;
+  }
   return document.getElementsByClassName('apphub_AppName')[0].innerText;
 }
 
-function getSearchUrl(store){
-  if(store === 'gog'){
-    return "https://www.gog.com/games?sort=bestselling&search=" + getAppName();
-  }
-  else {
-    return "https://isthereanydeal.com/#/search:"+ getAppName() +";/scroll:%23gamelist"
-  }
-}
 
-function createLink() {
-  var game_area = document.createElement('div');
-  game_area.className += "details_block"
+function createLink(store){
+  var alternative = document.createElement('div');
+  alternative.className += 'details_block';
   var link = document.createElement('a');
-  link.className += "linkbar";
-  link.innerText = "IsThereAnyDeal";
-  link.href = getSearchUrl();
-  var link2 = document.createElement('a');
-  link2.className += "linkbar";
-  link2.innerText = "DRM Free (GOG)";
-  link2.href = getSearchUrl("gog");
-
-
+  link.className += 'linkbar';
   
-  var header = document.createElement('div');
-  header.innerText = 'Other stores:';
-  header.className += "block_title";
-  game_area.appendChild(header);
-  game_area.appendChild(link);
-  game_area.appendChild(link2);
+  if(store === 'isthereanydeal'){
+    link.innerText = 'IsThereAnyDeal';
+    link.href = 'https://isthereanydeal.com/#/search:' + getAppName() + ';/scroll:%23gamelist'
+  }
 
-  return game_area;
+  if(store === 'gog') {
+    link.innerText = 'DRM Free (GOG)';
+    link.href = 'https://www.gog.com/games?sort=bestselling&search=' + getAppName();
+  }
+
+  alternative.appendChild(link);
+  return alternative;
+
+}
+
+function createHeader(){
+  var header = document.createElement('div');
+  header.className += 'block_title';
+  header.innerText = 'Other stores:';
+  return header;
+ 
+}
+
+function createDisplayArea(){
+  var displayArea = document.createElement('div');
+  displayArea.className += 'block responsive_apppage_details_right';
+  displayArea.appendChild(createHeader());
+  displayArea.appendChild(createLink('isthereanydeal'));
+  displayArea.appendChild(createLink('gog'));
+  return displayArea;
 }
 
 
-var header = document.createElement('div');
-var link = document.createElement('a');
-link.href= getSearchUrl(getAppName());
-header.appendChild(createLink());
-header.className += "block responsive_apppage_details_right"
-link.textContent = "IsThereAnyDeal";
+function insertIntoDOM(child){
+  var rightColumn = document.body.getElementsByClassName('rightcol game_meta_data');
+  if(rightColumn.length === 0 || !rightColumn[0].firstChild){
+    return;
+  }
 
+  rightColumn[0].insertBefore(child, rightColumn[0].firstChild);
+}
 
+// If vital information is missing, then hide the addon
+function shouldAddonRun(){
+  if(!getAppName()){
+    return false;
+  } 
+ return true; 
+}
 
-var rightColumn = document.body.getElementsByClassName('rightcol game_meta_data');
-rightColumn[0].insertBefore(header, rightColumn[0].firstChild);
-
+if(shouldAddonRun()){
+  var displayArea = createDisplayArea();
+  insertIntoDOM(displayArea);
+}
